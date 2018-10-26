4b027f2acc0222b7865e22aff6da1627ce4953cf
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-26 15:49:56 +0800

    try to avoid "The OpenGL functionality tests failed!"

--- scripts/build.qt.5.11.1.static.ubuntu.1604.sh
+++ scripts/build.qt.5.11.1.static.ubuntu.1604.sh
@@ -1,5 +1,19 @@
 wget https://download.qt.io/archive/qt/5.11/5.11.1/single/qt-everywhere-src-5.11.1.tar.xz
 tar -Jxf qt-everywhere-src-5.11.1.tar.xz
 cd qt-everywhere-src-5.11.1
-./configure -static -opensource -confirm-license
+
+
+
+
+##### note
+### -no-opengl
+###      try to avoid:
+###          ERROR: The OpenGL functionality tests failed!
+###          You might need to modify the include and library search paths by editing QMAKE_INCDIR_OPENGL[_ES2],
+###          QMAKE_LIBDIR_OPENGL[_ES2] and QMAKE_LIBS_OPENGL[_ES2] in the mkspec for your platform.
+
+./configure -opensource -confirm-license -static -no-opengl
+
+
+
 make -j
